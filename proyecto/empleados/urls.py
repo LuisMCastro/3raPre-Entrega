fe12342fe2f8fd621dc369652cd9b4b1fad91843
Luis Castro <luismcastro99@gmail.com>
2024-05-16 19:56:30 -0300

    feat(empleados): Terminado CRUD de Seccion.

--- proyecto/empleados/urls.py
+++ proyecto/empleados/urls.py
@@ -7,4 +7,6 @@ app_name = "empleados"
 urlpatterns = [
     path("lista/", views.seccion_list, name="seccion_list"),
     path("crear/", views.seccion_create, name="seccion_create"),
+    path("borrar/<int:pk>", views.seccion_delete, name="seccion_delete"),
+    path("modificar/<int:pk>", views.seccion_update, name="seccion_update"),
 ]
--- proyecto/empleados/views.py
+++ proyecto/empleados/views.py
@@ -19,3 +19,23 @@ def seccion_create(request):
     else:
         form = SeccionForm()
     return render(request, "empleados/seccion_form.html", {"form": form})
+
+
+def seccion_delete(request, pk: int):
+    consulta = Seccion.objects.get(id=pk)
+    if request.method == "POST":
+        consulta.delete()
+        return redirect("empleados:seccion_list")
+    return render(request, "empleados/seccion_delete.html", {"object": consulta})
+
+
+def seccion_update(request, pk: int):
+    consulta = Seccion.objects.get(id=pk)
+    if request.method == "POST":
+        form = SeccionForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect("empleados:seccion_list")
+    else:
+        form = SeccionForm(instance=consulta)
+    return render(request, "empleados/seccion_form.html", {"form": form})
